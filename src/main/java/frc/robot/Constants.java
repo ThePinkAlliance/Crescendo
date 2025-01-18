990c2021bad9fb00e020c59510f11c03f08b0fd2
code4first <105398626+TopHatTacco@users.noreply.github.com>
2025-01-17 19:44:06 -0500

    update  calibration using new swerve modules on FL and BL

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -119,12 +119,12 @@ public class Constants {
 
         /**
          * These values where determined by lining up all the wheels and recording the
-         * outputed positions.
+         * outputed positions with the turning gears facing Right when viewing from the back.
          */
-        public static final double kFrontLeftDriveAbsoluteEncoderOffsetRad = -1.52;// 2.688;
-        public static final double kBackLeftDriveAbsoluteEncoderOffsetRad = 2.47;// -1.7185;
-        public static final double kFrontRightDriveAbsoluteEncoderOffsetRad = 0.97;// 0.182;
-        public static final double kBackRightDriveAbsoluteEncoderOffsetRad = 1.92;// -2.519;
+        public static final double kFrontLeftDriveAbsoluteEncoderOffsetRad = -1.43; //-1.52;// 2.688;
+        public static final double kBackLeftDriveAbsoluteEncoderOffsetRad = 2.635; //2.47;// -1.7185;
+        public static final double kFrontRightDriveAbsoluteEncoderOffsetRad = 0.985; //0.97;// 0.182;
+        public static final double kBackRightDriveAbsoluteEncoderOffsetRad = 2.01; //1.92;// -2.519;
 
         public static final double kPhysicalMaxSpeedMetersPerSecond = 4.437;// 2.91;
         public static final double kPhysicalMaxAngularSpeedRadiansPerSecond = 18; // 18 rad/sec
@@ -132,7 +132,7 @@ public class Constants {
         public static final double kTeleDriveMaxSpeedMetersPerSecond = kPhysicalMaxSpeedMetersPerSecond * 1; // 0.96
         public static final double kTeleDriveMaxAngularSpeedRadiansPerSecond = kPhysicalMaxAngularSpeedRadiansPerSecond;
         public static double kTeleDriveSpeedReduction = 1;
-        public static final double kTeleDriveMaxAccelerationUnitsPerSecond = kPhysicalMaxSpeedMetersPerSecond * 0.85;
+        public static final double kTeleDriveMaxAccelerationUnitsPerSecond = kPhysicalMaxSpeedMetersPerSecond * 0.4; //0.85;
         public static final double kTeleDriveMaxAngularAccelerationUnitsPerSecond = 3.5;
     }
 }
